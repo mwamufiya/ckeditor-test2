558d670b4536f151969ea8fff5f88fde33dd68cb
Moof <mwamufiya@gmail.com>
2023-03-08 12:57:53 -0500

    add transformignore patterns

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,15 @@
 const nxPreset = require('@nrwl/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = { 
+  ...nxPreset,
+  transform: {
+    '^.+\\.(js)$': 'babel-jest',
+    'node_modules/@ckeditor/.+\.(js|ts)$': 'babel-jest',
+    '^.+\\.(ts|mjs|js|html|svg)$': 'jest-preset-angular'
+  },
+  transformIgnorePatterns: [
+    
+    '/node_modules/(?!(.*.mjs)|(lodash-es)|(@ckeditor/.+\.js))',
+    '\\.pnp\\.[^\\/]+$'
+  ]
+};
